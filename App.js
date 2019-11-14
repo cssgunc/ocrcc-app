434e6f1bc96d64ba41d9cba5ac5bc08acf5a361e
Mary <mary.luong21@yahoo.com>
2019-11-14 18:58:36 -0500

    mapss

--- App.js
+++ App.js
@@ -1,101 +1,3 @@
-<<<<<<< HEAD
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React, {Fragment} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
-
-const App = () => {
-  return (
-    <View style={styles.container}>
-     <MapView
-       provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-       style={styles.map}
-       region={{
-         latitude: 37.78825,
-         longitude: -122.4324,
-         latitudeDelta: 0.015,
-         longitudeDelta: 0.0121,
-       }}
-     >
-     </MapView>
-   </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFillObject,
-    height: 400,
-    width: 400,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
- });
-
-// const styles = StyleSheet.create({
-//   scrollView: {
-//     backgroundColor: Colors.lighter,
-//   },
-//   engine: {
-//     position: 'absolute',
-//     right: 0,
-//   },
-//   body: {
-//     backgroundColor: Colors.white,
-//   },
-//   sectionContainer: {
-//     marginTop: 32,
-//     paddingHorizontal: 24,
-//   },
-//   sectionTitle: {
-//     fontSize: 24,
-//     fontWeight: '600',
-//     color: Colors.black,
-//   },
-//   sectionDescription: {
-//     marginTop: 8,
-//     fontSize: 18,
-//     fontWeight: '400',
-//     color: Colors.dark,
-//   },
-//   highlight: {
-//     fontWeight: '700',
-//   },
-//   footer: {
-//     color: Colors.dark,
-//     fontSize: 12,
-//     fontWeight: '600',
-//     padding: 4,
-//     paddingRight: 12,
-//     textAlign: 'right',
-//   },
-// });
-=======
 import React, { Component } from 'react';
 //import react in our code.
 import { View, Image, TouchableOpacity, Text, Dimensions } from 'react-native';
@@ -113,7 +15,8 @@ import MenuDrawer from './components/MenuDrawer'
 import HomeScreen from './screens/HomeScreen';
 import LinksScreen from './screens/LinksScreen';
 import SettingsScreen from './screens/SettingsSC';
-import PinPage from './screens/PinPage'
+import PinPage from './screens/PinPage';
+import MapView from './screens/MapScreen'
 class NavigationDrawerStructure extends Component {
 
   //Structure for the navigatin Drawer
@@ -200,7 +103,22 @@ const SettingsScreen_StackNavigator = createStackNavigator({
     }),
   },
 });
->>>>>>> mapBranch
+
+const MapScreen_StackNavigator = createStackNavigator({
+  //All the screen from the HomeScreen will be indexed here
+  Pin: {
+    screen: MapScreen,
+    navigationOptions: ({ navigation }) => ({
+      title: appTitle,
+      headerLeft: <NavigationDrawerStructure navigationProps={navigation} />,
+      headerStyle: {
+        backgroundColor: '#008c99',
+
+      },
+      headerTintColor: '#fff',
+    }),
+  },
+});
 
 const WIDTH = Dimensions.get('window').width;
 
@@ -244,8 +162,15 @@ const DrawerNavigator = createDrawerNavigator(
         drawerLabel: 'Settings',
       },
     },
+    MapScreen: {
+      screen: MapScreen_StackNavigator,
+      navigationOptions: {
+        drawerLabel: 'Map',
+      },
+    }
   },
   DrawerConfig
 );
+
 
 export default createAppContainer(DrawerNavigator);
